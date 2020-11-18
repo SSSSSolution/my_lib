88d60a2aede2a0fbb13582685999b95fedc7bfa8
wei.huang <1845739048@qq.com>
2020-11-19 00:34:05 +0800

    1.Change RLog class's all member to static. 	2.Add output file setting. 	modified:   log/src/main.cpp 	modified:   log/src/rlog.cpp 	modified:   log/src/rlog.h

--- log/src/rlog.h
+++ log/src/rlog.h
@@ -1,27 +1,65 @@
 #ifndef RLOG_H
 #define RLOG_H
+#include <stdio.h>
 
-#define RLOG(severity, fmt, ...)   \
-  do { \
-    rlog::rlog_print(rlog::Severity::severity, #severity, __FILE__, __FUNCTION__, __LINE__, fmt"\n", __VA_ARGS__); \
+#define RLOG(severity, fmt, ...)                                            \
+  do {                                                                      \
+    rlog::RLog::print(rlog::Severity::severity,             \
+      #severity, __FILE__, __FUNCTION__, __LINE__, fmt, ## __VA_ARGS__);    \
   } while(0)
 
 
 namespace rlog {
 // https://stackoverflow.com/questions/2031163/when-to-use-the-different-log-levels
-enum Severity {
+  enum Severity {
     TRACE,
     DEBUG,
     INFO,
     WARN,
     ERROR,
     FATAL,
-};
+  };
 
-void rlog_print(Severity severity_level, const char *severity, const char *file, const char *func, int line, const char *fmt, ...);
+  class RLog
+  {
+  public:
+    static void set_severity_level(Severity s);
+    static void set_target(FILE *fp);
+    static void set_target(const char *filename);
+
+    static void print(Severity severity_level, const char *severity, const char *file,
+               const char *func, int line, const char *fmt, ...);
+
+
+  private:
+    static Severity    m_severity_level;
+    static char        m_buf[1024];
+    static FILE        *m_fp;
+  };
 
 }
 
 
 
 #endif // RLOG_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
