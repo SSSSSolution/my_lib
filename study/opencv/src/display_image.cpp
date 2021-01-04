9b5c7e8f855be28a75fe1032410f8877706199cf
wei.huang <1845739048@qq.com>
2021-01-05 00:12:41 +0800

    lueluelue

--- study/opencv/src/display_image.cpp
+++ study/opencv/src/display_image.cpp
@@ -19,10 +19,19 @@ int main(int argc, char **argv)
         printf("No image data \n");
         return -1;
     }
-    cv::namedWindow("Display Image", WINDOW_FREERATIO);
+    cv::namedWindow("Display Image", WINDOW_GUI_EXPANDED);
     imshow("Display Image", image);
 
     waitKey(0);
+    Mat M(2, 2, CV_8UC3, Scalar(0, 0, 255));
+    std::cout << "M = " << std::endl << M << std::endl << std::endl;
+    Mat E = Mat::eye(4, 4, CV_64F);
+    std::cout << "eye = " << std::endl << E << std::endl;
 
+    Mat O = Mat::ones(2, 2, CV_32F);
+    std::cout << "O = " << std::endl << O << std::endl;
+
+    Mat Z = Mat::zeros(3, 3, CV_8UC1);
+    std::cout << "Z = " << std::endl << Z << std::endl;
     return 0;
 }
